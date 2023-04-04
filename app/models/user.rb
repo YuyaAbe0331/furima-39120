37c6639c8453e7bf9ed47f538dbba032c7fa2cf4
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-05 00:26:29 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :kana_first_name, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
+  validates :kana_first_name, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
+  validates :date_of_birth, presence: true
+
 end
