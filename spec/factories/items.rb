9e43c551159b0202fc88feface5cb861bddac887
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-06 22:12:38 +0900

    テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { Faker::Lorem.sentence }
+    content             { Faker::Lorem.paragraph }
+    category_id         { Faker::Number.between(from: 2, to: 11) }
+    status_id           { Faker::Number.between(from: 2, to: 8) }
+    delivery_id         { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id       { Faker::Number.between(from: 2, to: 48) }
+    duration_id         { Faker::Number.between(from: 2, to: 4) }
+    price               { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpeg')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,98 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品が出品できる場合' do
+      it 'image,name,content,category_id,status_id,delivery_id,prefecture_id,duration_id,priceが存在していれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない場合' do
+      it '画像がない場合出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Image can't be blank")
+      end
+      it '商品名がない場合出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Name can't be blank")
+      end
+      it '商品の説明がない場合出品できない' do
+        @item.content = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Content can't be blank")
+      end
+      it 'category_idが1の場合出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Category can't be blank")
+      end
+      it 'status_idが1の場合出品できない' do
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Status can't be blank")
+      end
+      it 'delivery_idが1の場合出品できない' do
+        @item.delivery_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Delivery can't be blank")
+      end
+      it 'prefecture_idが1の場合出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Prefecture can't be blank")
+      end
+      it 'duration_idが1の場合出品できない' do
+        @item.duration_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Duration can't be blank")
+      end
+      it 'priceがない場合出品できない' do
+        @item.price =''
+        binding.pry
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price can't be blank")
+      end
+      it 'priceが299円以下の場合出品できない' do
+        @item.price ='299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price must be greater than or equal to 300")
+      end
+      it 'priceが10000000円以上の場合出品できない' do
+        @item.price ='10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price must be less than or equal to 9999999")
+      end
+      it 'priceが全角数字だと出品できない' do
+        @item.price ='５０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is not a number")
+      end
+      it 'priceがひらがなだと出品できない' do
+        @item.price ='あいうえお'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is not a number")
+      end
+      it 'priceがカタカナだと出品できない' do
+        @item.price ='アイウエオ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is not a number")
+      end
+      it 'priceが漢字だと出品できない' do
+        @item.price ='漢字漢字漢字'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is not a number")
+      end
+      it 'priceが英語だと出品できない' do
+        @item.price ='abcdef'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is not a number")
+      end
+    end
+  end
 end
