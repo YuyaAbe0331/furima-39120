fe3e6bee82b998980624ae67ab0a4f2365b85cb8
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-06 17:10:51 +0900

    アソシエーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :duration
+
+  validates :image, presence: true, unless: :was_attached?
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :duration_id, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+
+
+  def was_attached?
+    image.attached?
+  end
 end
