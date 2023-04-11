2e99779b5c71eb681767513bf9635e8251ae4113
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-11 20:52:13 +0900

    バリデーションの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,6 +8,11 @@ class PurchaseAddress
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :municipality, presence: true
   validates :address, presence: true
-  validates :telephone, presence: true format: {with: /\A\d{10,11}\z/, "Input only number"}
+  validates :telephone, presence: true, format: {with: /\A\d{10,11}\z/, message: "Input only number"}
+
+  def save
+    @purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    @address = Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, telephone: telephone, purchase_id: @purchase.id)
+  end
 
 end
