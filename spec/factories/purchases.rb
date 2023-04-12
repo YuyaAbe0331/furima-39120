204b70ea1de10cb0936ea04cd853eaa656ec8094
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-13 01:38:22 +0900

    FactoryBotの設定

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :purchase do
-    token {"tok_abcdefghijk00000000000000000"}
+    token       {"tok_abcdefghijk00000000000000000"}
     association :user
     association :item 
   end
