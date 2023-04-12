3a00b2eff76c546f408f83a8e87c86d4307186c5
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-12 18:48:38 +0900

    payjpを用いた購入アクションの定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -14,6 +14,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -24,6 +25,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :building, :telephone).merge(user_id: current_user.id, item_id:  params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :building, :telephone).merge(user_id: current_user.id, item_id:  params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
