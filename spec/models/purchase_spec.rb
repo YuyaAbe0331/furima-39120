431082f5a042b718e4ac5c1451e22231f649c279
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-12 18:46:32 +0900

    payjpの導入

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase = FactoryBot.build(:purchase)
+  end
+  describe '商品購入' do
 end
