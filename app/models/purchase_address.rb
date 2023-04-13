3c47219ca86c94acbe94ee74b0acfbaa4f11ccc0
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-13 14:09:42 +0900

    バリデーションのリファクタリング

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,14 +2,16 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building, :telephone, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :post_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Include hyphen(-)' }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :address, presence: true
-  validates :telephone, presence: true, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
-  validates :token, presence: true
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Include hyphen(-)' }
+    validates :municipality
+    validates :address
+    validates :telephone, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+    validates :token
+  end
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     @purchase = Purchase.create(user_id: user_id, item_id: item_id)
